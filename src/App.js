2a12a51dc819561c03e6e405bbacbc43d912e79e
Wes-Fielder <wesfielder1@gmail.com>
2021-09-22 00:19:22 -0500

    Add button component and basic demo layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Button/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="title-div">Buttons</div>
+      <div className="small-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">&#60;Button /&#62;</span>
+          <div>
+            <Button />
+          </div>
+        </div>
+        <div className="button-display">
+          <span className="subtitle-div">&:hover, &:focus</span>
+          <div>
+            <Button className="default-color-hover-focus" />
+          </div>
+        </div>
+      </div>
+
+      <div className="small-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">
+            &#60;Button variant="outline" /&#62;
+          </span>
+          <div>
+            <Button buttonType="outline" />
+          </div>
+        </div>
+        <div className="button-display">
+          <span className="subtitle-div">&:hover, &:focus</span>
+          <div>
+            <Button
+              buttonType="outline"
+              className="outline-button-hover-focus"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="small-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">
+            &#60;Button variant="text" /&#62;
+          </span>
+          <div>
+            <Button buttonType="text" />
+          </div>
+        </div>
+        <div className="button-display">
+          <span className="subtitle-div">&:hover, &:focus</span>
+          <div>
+            <Button buttonType="text" className="text-button-hover-focus" />
+          </div>
+        </div>
+      </div>
+
+      <div className="small-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">&#60;Button disableShadow /&#62;</span>
+          <div>
+            <Button boxShadow="disabled" className="box-shadow-example-color" />
+          </div>
+        </div>
+      </div>
+
+      <div className="small-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">&#60;Button disabled /&#62;</span>
+          <div>
+            <Button disabled={true} />
+          </div>
+        </div>
+
+        <div className="button-display">
+          <span className="subtitle-div">
+            &#60;Button variant="text" disabled /&#62;
+          </span>
+          <div>
+            <Button buttonType="text" disabled />
+          </div>
+        </div>
+      </div>
+
+      <div className="medium-col-button-area">
+        <div className="button-display">
+          <span className="subtitle-div">
+            &#60;Button startIcon="local_grocery_store" /&#62;
+          </span>
+          <div>
+            <Button startIcon="local_grocery_store" color="primary" />
+          </div>
+        </div>
+
+        <div className="button-display">
+          <span className="subtitle-div">
+            &#60;Button endIcon="local_grocery_store" /&#62;
+          </span>
+          <div>
+            <Button endIcon="local_grocery_store" color="primary" />
+          </div>
+        </div>
+      </div>
+
+      <div className="large-col-button-area">
+        <div className="large-button-display">
+          <span className="subtitle-div">&#60;Button size="sm" /&#62;</span>
+          <div>
+            <Button size="sm" color="primary" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <span className="subtitle-div">&#60;Button size="md" /&#62;</span>
+          <div>
+            <Button size="md" color="primary" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <span className="subtitle-div">&#60;Button size="lg" /&#62;</span>
+          <div>
+            <Button size="lg" color="primary" />
+          </div>
+        </div>
+      </div>
+
+      <div className="large-col-button-area">
+        <div className="large-button-display">
+          <span className="subtitle-div">
+            &#60;Button color="default" /&#62;
+          </span>
+          <div>
+            <Button color="default" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <span className="subtitle-div">
+            &#60;Button color="primary" /&#62;
+          </span>
+          <div>
+            <Button color="primary" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <span className="subtitle-div">
+            &#60;Button color="secondary"/&#62;
+          </span>
+          <div>
+            <Button color="secondary" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <span className="subtitle-div">
+            &#60;Button color="danger" /&#62;
+          </span>
+          <div>
+            <Button color="danger" />
+          </div>
+        </div>
+      </div>
+
+      <div className="large-col-button-area">
+        <div className="large-button-display">
+          <span className="subtitle-div">&:hover, &:focus</span>
+          <div>
+            <Button color="default" className="default-color-hover-focus" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <div>
+            <Button color="primary" className="primary-color-hover-focus" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <div>
+            <Button color="secondary" className="secondary-color-hover-focus" />
+          </div>
+        </div>
+
+        <div className="large-button-display">
+          <div>
+            <Button color="danger" className="danger-color-hover-focus" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Button/Button.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "./Button.css";
+import shopping_cart from "./button-icons/add_shopping_cart_white_24dp.svg";
+import cancel_icon from "./button-icons/cancel_white_24dp.svg";
+import delete_icon from "./button-icons/delete_outline_white_24dp.svg";
+import help_icon from "./button-icons/help_white_24dp.svg";
+import pets_icon from "./button-icons/pets_white_24dp.svg";
+
+const Button = (props) => {
+  const buttonType = props.buttonType;
+  const boxShadow = props.boxShadow;
+  const disabled = props.disabled;
+  const startIcon = props.startIcon;
+  const endIcon = props.endIcon;
+  const buttonSize = props.size;
+  const color = props.color;
+
+  let buttonText = disabled ? "Disabled" : "Default";
+
+  let styleClasses = ["button-base", "box-shadow"];
+
+  // set size
+  if (buttonSize === "sm") {
+    styleClasses.push("size-sm");
+  } else if (buttonSize === "lg") {
+    styleClasses.push("size-lg");
+  } else {
+    styleClasses.push("size-md");
+  }
+
+  // set color
+  if (color === "primary") {
+    styleClasses.push("primary-color");
+  } else if (color === "secondary") {
+    styleClasses.push("secondary-color");
+  } else if (color === "danger") {
+    styleClasses.push("danger-color");
+  } else {
+    styleClasses.push("default-color");
+  }
+
+  // set button type
+  if (buttonType === "outline") {
+    styleClasses.push("outline-button");
+  }
+
+  if (buttonType === "text") {
+    styleClasses.push("text-button");
+    styleClasses = styleClasses.filter((v) => v !== "box-shadow");
+  }
+
+  if (boxShadow === "disabled") {
+    styleClasses = styleClasses.filter((v) => v !== "box-shadow");
+  }
+
+  // add icons
+  let preIcon = "";
+  if (startIcon === "local_grocery_store") {
+    preIcon = shopping_cart;
+  } else if (startIcon === "cancel") {
+    preIcon = cancel_icon;
+  } else if (startIcon === "delete") {
+    preIcon = delete_icon;
+  } else if (startIcon === "help") {
+    preIcon = help_icon;
+  } else if (startIcon === "pets") {
+    preIcon = pets_icon;
+  }
+
+  let afterIcon = "";
+  if (endIcon === "local_grocery_store") {
+    afterIcon = shopping_cart;
+  } else if (endIcon === "cancel") {
+    afterIcon = cancel_icon;
+  } else if (endIcon === "delete") {
+    afterIcon = delete_icon;
+  } else if (endIcon === "help") {
+    afterIcon = help_icon;
+  } else if (endIcon === "pets") {
+    afterIcon = pets_icon;
+  }
+
+  // add custom class to display hover and focus states
+  styleClasses.push(props.className);
+
+  return (
+    <button className={styleClasses.join(" ")} disabled={disabled}>
+      <div className="button-content">
+        {startIcon ? (
+          <img className="button-img" src={preIcon} alt="start icon"></img>
+        ) : null}
+        {buttonText}
+        {endIcon ? (
+          <img className="button-img" src={afterIcon} alt="end icon"></img>
+        ) : null}
+      </div>
+    </button>
+  );
+};
+
+export default Button;
